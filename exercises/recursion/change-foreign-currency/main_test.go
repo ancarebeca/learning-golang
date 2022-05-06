2d39e9c7925dc3d52d939bdd3c5eee6362c3c7ff
ancarebeca <anca.rebeca@gmail.com>
2022-05-06 18:52:17 +0100

    rename test

--- exercises/recursion/change-foreign-currency/main_test.go
+++ exercises/recursion/change-foreign-currency/main_test.go
@@ -12,7 +12,7 @@ func TestCanGetExactChangeBruteForce(t *testing.T) {
 	assert.True(t, canGetExactChangeBruteForce(9, []int{5, 3, 2}))
 }
 
-func TestCanGetExactChangeBruteMemoization(t *testing.T) {
+func TestCanGetExactVersion2(t *testing.T) {
 	assert.True(t, canGetExactChangeVersion2(75, []int{4, 17, 29}))
 	assert.False(t, canGetExactChangeVersion2(94, []int{5, 10, 25, 100, 200}))
 	assert.True(t, canGetExactChangeVersion2(7, []int{1, 3, 4, 5}))
