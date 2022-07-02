fef12f4d73219e6c1c40ab0cb20da1f1366dd8b5
ancarebeca <anca.rebeca@gmail.com>
2022-07-02 17:31:23 +0100

    bst operation

--- exercises/trees/bst/main.go
+++ exercises/trees/bst/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Node struct {
-	key   int
-	right *Node
-	left  *Node
+	Val   int
+	Right *Node
+	Left  *Node
 }
 
 type BST struct {
@@ -13,23 +16,405 @@ type BST struct {
 }
 
 func (bst *BST) insert(key int) {
-	bst.root = bst.insertRec(bst.root, key)
+	bst.root = insertRec(bst.root, key)
 }
 
-func (bst *BST) insertRec(node *Node, key int) *Node {
-
+// Time O(n) where n is the number of nodes
+// Space O(n) with 'n' being the depth of the tree since at any point of time maximum number of stack frames that could
+// be present in memory is 'n'.
+func insertRec(node *Node, value int) *Node {
 	if node == nil {
-		return &Node{key: key}
+		return &Node{
+			Val: value,
+		}
 	}
 
-	if key < node.key {
-		node.left = bst.insertRec(node.left, key)
+	if value > node.Val {
+		node.Right = insertRec(node.Right, value)
 	}
-	if key > node.key {
-		node.right = bst.insertRec(node.right, key)
+
+	if value < node.Val {
+		node.Left = insertRec(node.Left, value)
 	}
 
 	return node
+}
+
+//  Delete
+// Time: O(n)
+// Space: O(n)
+func delete(root *Node, value int) *Node {
+
+	// base case: the value is not found in the tree
+	if root == nil {
+		return root
+	}
+
+	// if the given value is less than the root node, recur for the left subtree
+	if value < root.Val {
+		root.Left = delete(root.Left, value)
+	}
+
+	// if the given value is bigger than the root node, recur for the right subtree
+	if value > root.Val {
+		root.Right = delete(root.Right, value)
+	}
+
+	// value found
+	if root.Val == value {
+		//Case 1: node to be deleted has no children (it is a leaf node)
+		if root.Left == nil && root.Right == nil {
+			return nil
+		}
+		//Case 2: Deleting a node with one child: remove the node and replace it with its child.
+		if root.Left == nil && root.Right != nil {
+			return root.Right
+		}
+
+		if root.Left != nil && root.Right == nil {
+			return root.Left
+		}
+		// Case3: Deleting a node with two children.
+		tempNode := minValued(root.Right)             // Find min right sub-tree
+		root.Val = tempNode.Val                       // Copy the value in targeted node
+		root.Right = delete(root.Right, tempNode.Val) // Delete duplicate from right subtree
+	}
+
+	return root
+}
+
+func minValued(root *Node) *Node {
+	temp := root
+	for nil != temp && temp.Left != nil {
+		temp = temp.Left
+	}
+	return temp
+}
+
+// Time: O(n)
+// Space: O(n)
+func breadthFirstSearch(node *Node) []int {
+	var output []int
+	var queue []*Node
+	queue = append(queue, node)
+
+	for len(queue) > 0 {
+		current := queue[0]
+		output = append(output, current.Val)
+		queue = queue[1:]
+
+		if current.Left != nil {
+			queue = append(queue, current.Left)
+		}
+
+		if current.Right != nil {
+			queue = append(queue, current.Right)
+		}
+	}
+
+	return output
+}
+
+func (bst *BST) breadthFirstSearchRec() {
+	var q queue
+	q.add(bst.root)
+	bst.searchRec(q)
+}
+
+func (bst *BST) searchRec(q queue) {
+
+	if len(q.items) <= 0 {
+		return
+	}
+
+	current := q.pop()
+	fmt.Println(current.Val)
+
+	if current.Left != nil {
+		q.add(current.Left)
+	}
+
+	if current.Right != nil {
+		q.add(current.Right)
+	}
+
+	bst.searchRec(q)
+}
+
+// ---- Depth-first searchRec
+
+// Pre-order
+func dfsPreOrder(node *Node) []int {
+	var output []int
+	stack := []*Node{}
+	stack = append(stack, node)
+
+	for len(stack) > 0 {
+
+		current := stack[len(stack)-1]
+		output = append(output, current.Val)
+		stack = stack[:len(stack)-1]
+
+		if current.Right != nil {
+			stack = append(stack, current.Right)
+		}
+
+		if current.Left != nil {
+			stack = append(stack, current.Left)
+		}
+
+	}
+
+	return output
+}
+
+func dfsPreOrderRec(node *Node) []int {
+	if node == nil {
+		return nil
+	}
+
+	var l, r, o []int
+	if node.Left != nil {
+		l = dfsPreOrderRec(node.Left)
+	}
+
+	if node.Right != nil {
+		r = dfsPreOrderRec(node.Right)
+	}
+	o = append(o, node.Val)
+	o = append(o, l...)
+	o = append(o, r...)
+
+	return o
+}
+
+// In-order
+func dfsInorder(node *Node) []int {
+	var output []int
+	stack := []*Node{}
+
+	current := node
+	for len(stack) > 0 || current != nil {
+		if current != nil {
+			stack = append(stack, current)
+			current = current.Left
+			continue
+		}
+
+		current = stack[len(stack)-1]
+		output = append(output, current.Val)
+		current = current.Right
+		stack = stack[:len(stack)-1]
+	}
+
+	return output
+}
+
+func dfsInorderRec(node *Node) []int {
+	var o []int
+	if node == nil {
+		return nil
+	}
+
+	left := dfsInorderRec(node.Left)
+	right := dfsInorderRec(node.Right)
+	o = append(o, left...)
+	o = append(o, node.Val)
+	o = append(o, right...)
+	return o
+}
+
+// Post-oder
+func dfsPostOrderRec(node *Node) []int {
+	var o []int
+	if node == nil {
+		return nil
+	}
+
+	left := dfsPostOrderRec(node.Left)
+
+	right := dfsPostOrderRec(node.Right)
+
+	o = append(o, left...)
+	o = append(o, right...)
+	o = append(o, node.Val)
+
+	return o
+}
+
+func dfsPostOrder(node *Node) []int {
+	var o []int
+	if node == nil {
+		return nil
+	}
+
+	left := dfsPostOrderRec(node.Left)
+
+	right := dfsPostOrderRec(node.Right)
+
+	o = append(o, left...)
+	o = append(o, right...)
+	o = append(o, node.Val)
+
+	return o
+}
+
+// Height
+func getHeight(node *Node) int {
+	var q []*Node
+	q = append(q, node)
+	height := 0
+
+	for len(q) > 0 {
+		// calculate the total number of nodes at the current level
+		size := len(q)
+
+		for size > 0 {
+			current := q[0]
+			q = q[1:]
+
+			if current.Left != nil {
+				q = append(q, current.Left)
+			}
+
+			if current.Right != nil {
+				q = append(q, current.Right)
+			}
+			size--
+		}
+		// increment height by 1 for each level
+		height++
+	}
+
+	return height
+}
+
+func getHeightRec(node *Node) int {
+	if node == nil {
+		return -1
+	}
+
+	left := getHeightRec(node.Left)
+	right := getHeightRec(node.Right)
+
+	return 1 + max(left, right)
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+// Find the node for the given node value
+func find(root *Node, value int) *Node {
+
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == value {
+		return root
+	}
+
+	if value < root.Val {
+		return find(root.Left, value)
+	}
+
+	if value > root.Val {
+		return find(root.Right, value)
+	}
+
+	return nil
+}
+
+// Check if BST is a balanced tree
+func isBalanced(root *Node) bool {
+	// To check if a tree is height-balanced, get the height of left and right subtrees. Return true if difference between
+	// heights is not more than 1 and left and right subtrees are balanced, otherwise return false.
+	if root == nil {
+		return true
+	}
+
+	lh := getHeightRec(root.Left)
+	rh := getHeightRec(root.Right)
+
+	if math.Abs(float64(lh)-float64(rh)) > 1 {
+		return false
+	}
+
+	if isBalanced(root.Left) && isBalanced(root.Right) == true {
+		return true
+
+	}
+
+	// if we reach here means tree is not
+	// height-balanced tree
+	return false
+}
+
+func isBalancedOptimised(root *Node) bool {
+	return isBalancedHelper(root) != -1
+}
+
+func isBalancedHelper(root *Node) int {
+	if root == nil {
+		return 0
+	}
+
+	lb := isBalancedHelper(root.Left)
+	if lb < 0 {
+		return -1
+	}
+
+	rb := isBalancedHelper(root.Right)
+	if rb < 0 {
+		return -1
+	}
+
+	if math.Abs(float64(lb)-float64(rb)) > 1 {
+		return -1
+	}
+
+	return max(lb, rb) + 1
+}
+
+// Convert a normal BST to Balanced BST
+// 1) Traverse given BST in inorder and store result in an array.
+// 2) Build a balanced BST from the above created sorted array:
+// 		2.1) Get the Middle of the array and make it root.
+// 		2.2) Recursively do same for left half and right half.
+//       a) Get the middle of left half and make it left child of the root
+//          created in step 2.1.
+//       b) Get the middle of right half and make it right child of the
+//          root created in step 2.1
+func balanceBST(root *Node) *Node {
+	values := dfsInorderRec(root)
+	return sortedArrayToBST(values, 0, len(values)-1)
+}
+
+func sortedArrayToBST(arr []int, start, end int) *Node {
+	/* Base Case */
+	if start > end {
+		return nil
+	}
+
+	/* Get the middle element and make it root */
+	mid := (start + end) / 2
+
+	node := Node{Val: arr[mid]}
+
+	/* Recursively construct the left subtree and make it
+	   left child of root */
+	node.Left = sortedArrayToBST(arr, start, mid-1)
+
+	/* Recursively construct the right subtree and make it
+	   right child of root */
+	node.Right = sortedArrayToBST(arr, mid+1, end)
+
+	return &node
 }
 
 type queue struct {
@@ -49,76 +434,6 @@ func (q *queue) pop() *Node {
 	q.items = q.items[1:]
 
 	return el
-}
-
-// Breadth-first transversal
-func (bst *BST) bfsTransversal() {
-	var q queue
-	q.add(bst.root)
-
-	for len(q.items) > 0 {
-
-		current := q.pop()
-		fmt.Println(current.key)
-
-		if current.left != nil {
-			q.add(current.left)
-		}
-
-		if current.right != nil {
-			q.add(current.right)
-		}
-
-	}
-}
-
-func (bst *BST) bfsTransversalRec() {
-	var q queue
-	q.add(bst.root)
-	bst.bfsRec(q)
-}
-
-func (bst *BST) bfsRec(q queue) {
-
-	if len(q.items) <= 0 {
-		return
-	}
-
-	current := q.pop()
-	fmt.Println(current.key)
-
-	if current.left != nil {
-		q.add(current.left)
-	}
-
-	if current.right != nil {
-		q.add(current.right)
-	}
-
-	bst.bfsRec(q)
-}
-
-// Depth-first transversal
-
-// Preorder <root><left><right>
-func (bst *BST) preOrderTransversalRec() {
-	preOrderRec(bst.root)
-}
-
-func preOrderRec(node *Node) {
-	if node == nil {
-		return
-	}
-
-	fmt.Println(node.key)
-
-	if node.left != nil {
-		preOrderRec(node.left)
-	}
-
-	if node.right != nil {
-		preOrderRec(node.right)
-	}
 }
 
 type stack struct {
@@ -144,25 +459,3 @@ func (s *stack) pop() *Node {
 
 	return item
 }
-
-func (bst *BST) preOrderTransversal() {
-	var s stack
-	s.push(bst.root)
-
-	for len(s.items) > 0 {
-		n := s.pop()
-		fmt.Println(n.key)
-
-		if n.right != nil {
-			s.push(n.right)
-		}
-
-		if n.left != nil {
-			s.push(n.left)
-		}
-	}
-}
-
-// Inorder
-
-// Postorder
--- exercises/trees/bst/main_test.go
+++ exercises/trees/bst/main_test.go
@@ -1,22 +1,89 @@
 package main
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestInsertNode(t *testing.T) {
-	bst := BST{}
-	bst.insert(50)
-	bst.insert(30)
-	bst.insert(20)
-	bst.insert(40)
-	bst.insert(70)
-	bst.insert(60)
-	bst.insert(80)
+func TestInsertNodeAndTraversals(t *testing.T) {
+	bst := createTree([]int{50, 30, 20, 40, 70, 60, 80})
 
-	fmt.Println("---")
-	bst.preOrderTransversalRec()
-	fmt.Println("---")
-	bst.preOrderTransversal()
+	assert.Equal(t, []int{50, 30, 70, 20, 40, 60, 80}, breadthFirstSearch(bst.root))
+
+	assert.Equal(t, []int{50, 30, 20, 40, 70, 60, 80}, dfsPreOrder(bst.root))
+	assert.Equal(t, []int{50, 30, 20, 40, 70, 60, 80}, dfsPreOrderRec(bst.root))
+
+	assert.Equal(t, []int{20, 30, 40, 50, 60, 70, 80}, dfsInorder(bst.root))
+	assert.Equal(t, []int{20, 30, 40, 50, 60, 70, 80}, dfsInorderRec(bst.root))
+
+	assert.Equal(t, []int{20, 40, 30, 60, 80, 70, 50}, dfsPostOrder(bst.root))
+	assert.Equal(t, []int{20, 40, 30, 60, 80, 70, 50}, dfsPostOrderRec(bst.root))
+}
+
+func TestHeight(t *testing.T) {
+	bst := createTree([]int{50, 30, 20, 40, 70, 60, 80})
+	assert.Equal(t, 2, getHeight(bst.root))
+}
+
+func TestSearch(t *testing.T) {
+	bst := createTree([]int{50, 30, 20, 40, 70, 60, 80})
+
+	node := find(bst.root, 50)
+	assert.Equal(t, 50, node.Val)
+
+	node = find(bst.root, 30)
+	assert.Equal(t, 30, node.Val)
+
+	node = find(bst.root, 80)
+	assert.Equal(t, 80, node.Val)
+
+	node = find(bst.root, 90)
+	assert.Nil(t, node)
+}
+
+func TestDelete(t *testing.T) {
+	bst1 := createTree([]int{50, 30, 20, 40, 70, 60, 80})
+	delete(bst1.root, 50)
+	assert.Equal(t, []int{60, 30, 70, 20, 40, 80}, breadthFirstSearch(bst1.root))
+
+	bst2 := createTree([]int{50, 30, 20, 40, 70, 60, 80})
+	delete(bst2.root, 20)
+	assert.Equal(t, []int{50, 30, 70, 40, 60, 80}, breadthFirstSearch(bst2.root))
+
+	bst3 := createTree([]int{50, 30, 20, 40, 70, 60, 55, 80})
+	delete(bst3.root, 60)
+	assert.Equal(t, []int{50, 30, 70, 20, 40, 55, 80}, breadthFirstSearch(bst3.root))
+}
+
+func TestCheckBalanced(t *testing.T) {
+	bst := createTree([]int{50, 30, 80, 70, 90, 60, 75})
+	assert.False(t, isBalanced(bst.root))
+
+	bst2 := createTree([]int{29, 16, 36, 10, 17, 34, 35, 6, 11, 20, 22})
+	assert.False(t, isBalanced(bst2.root))
+
+	bst3 := createTree([]int{50, 30, 20, 40, 70, 60, 80})
+	assert.True(t, isBalanced(bst3.root))
+
+	assert.False(t, isBalancedOptimised(bst2.root))
+	assert.True(t, isBalancedOptimised(bst3.root))
+}
+
+func TestBalanceBST(t *testing.T) {
+	bst := createTree([]int{50, 30, 80, 70, 90, 60, 75})
+	balancesBST := balanceBST(bst.root)
+	assert.True(t, isBalancedOptimised(balancesBST))
+
+	bst2 := createTree([]int{29, 16, 36, 10, 17, 34, 35, 6, 11, 20, 22})
+	balancesBST2 := balanceBST(bst2.root)
+	assert.True(t, isBalancedOptimised(balancesBST2))
+
+}
+
+func createTree(values []int) BST {
+	bst := BST{}
+	for _, v := range values {
+		bst.insert(v)
+	}
+	return bst
 }
